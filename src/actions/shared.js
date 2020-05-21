386284ed045cce2efca07bbe3c239954fd3e9236
Connie Zhou <connie@Connies-MacBook-Pro.local>
2020-05-21 01:09:37 -0500

    added actions

---
+++ src/actions/shared.js
@@ -0,0 +1,17 @@
+import { receiveQuestions } from './questions'
+import { receiveUsers } from './users'
+import { setAuthedUser } from './authedUser'
+import { showLoading, hideLoading } from 'react-redux-loading'
+
+export function handleInitialData (userId) {
+    return (dispatch) => {
+      dispatch(showLoading())
+      return getInitialData()
+        .then(({ users, tweets }) => {
+          dispatch(receiveUsers(users))
+          dispatch(receiveQuestions(tweets))
+          dispatch(setAuthedUser(userId ? userId : null))
+          dispatch(hideLoading())
+        })
+    }
+  }
